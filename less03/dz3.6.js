9ec303b6944ee830bdce1dc04f2e115367e41ffc
SergeyAvram3nko <97534600+SergeyAvram3nko@users.noreply.github.com>
2022-01-19 12:41:06 +0200

    reshenie 3.6

---
+++ less03/dz3.6.js
@@ -0,0 +1,28 @@
+/*
+####Задача 6
+
+Сделайте функцию `isEven()`, которая параметром принимает целое число и проверяет: чётное оно или нет.
+Если чётное — функция возвращает `true`, если нечётное — `false`.
+Данная функция должна обязательно содержать проверку входного параметра, потому что принимать она может только число.
+
+*Чётные числа могут делится на 2 без остатка.*
+
+```js
+isEven(3); // false
+isEven(4); // true
+isEven('Content'); // Error: parameter type is not a Number
+```
+ */
+
+function isEven(n){
+    if(typeof (n) !== "number"){
+        throw new Error("Error: parameter type is not a Number");
+    }
+    if(n === 0){
+        return false;
+    }
+    return n % 2 === 0 ? true : false;
+}
+console.log(isEven(3));
+console.log(isEven(4));
+console.log(isEven('Content'));
